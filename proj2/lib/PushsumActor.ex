b394b84c7df3ade7460fa20f5f3ce536b489708c
namrathatippireddy <mgunn001@odu.edu>
2019-09-29 00:12:38 -0400

    pushing push sum

--- proj2/lib/PushsumActor.ex
+++ proj2/lib/PushsumActor.ex
@@ -1,71 +1,83 @@
-# defmodule GossipActor do
-#   use GenServer
-#
-#   def init(msg) do
-#     {:ok, %{"message" => msg, "count" => 0, "neighbors" => []}}
-#   end
-#
-#   def handle_cast({:set_neighbors, neighbors}, state) do
-#     {:noreply, Map.put(state, "neighbors", neighbors)}
-#   end
-#
-#   def handle_call({:get_neighbors}, _from, state) do
-#     {:reply, Map.fetch(state, "neighbors"), state}
-#   end
-#
-#   def handle_cast({:transmit_rumor}, state) do
-#     {:ok, message} = Map.fetch(state, "message")
-#     {:ok, neighbors} = Map.fetch(state, "neighbors")
-#
-#     if message != "" && length(neighbors) > 0 do
-#       _ = GenServer.cast(Enum.random(neighbors), {:receive_rumor, message, self()})
-#     end
-#
-#     {:noreply, state}
-#   end
-#
-#   def handle_cast({:receive_rumor, rumor, sender}, state) do
-#     {:ok, count} = Map.fetch(state, "count")
-#
-#     if count > 10 do
-#       _ = GenServer.cast(sender, {:terminate_neighbor, self()})
-#       {:noreply, state}
-#     else
-#       # when count<10 increment the counter and set his msg, if
-#       # But you know what he is incrementing the count even on blank msg :P
-#       state = Map.put(state, "count", count + 1)
-#       {:ok, existing_msg} = Map.fetch(state, "message")
-#
-#       if(existing_msg != "") do
-#         {:noreply, state}
-#       else
-#         {:noreply, Map.put(state, "message", rumor)}
-#       end
-#     end
-#   end
-#
-#   def handle_cast({:terminate_neighbor, neighbor}, state) do
-#     {:ok, neighbors} = Map.fetch(state, "neighbors")
-#     {:noreply, Map.put(state, "neighbors", List.delete(neighbors, neighbor))}
-#   end
-#
-#   def handle_call({:get_count}, _from, state) do
-#     {:reply, Map.fetch(state, "count"), state}
-#   end
-#
-#   def handle_call({:get_message}, _from, state) do
-#     {:reply, Map.fetch(state, "message"), state}
-#   end
-#
-#   def handle_call({:get_neighbors_count}, _from, state) do
-#     {:reply, length(Map.fetch(state, "neighbors")), state}
-#   end
-#
-#   def handle_call({:set_message}, _from, state) do
-#     {:reply, Map.put(state, "message", state)}
-#   end
-#
-#   def set_neighbors(actor, neighbors) do
-#     GenServer.cast(actor, {:set_neighbors, neighbors})
-#   end
-# end
+defmodule PushsumActor do
+  use GenServer
+
+  def init(s) do
+
+    {:ok, %{"s" => s, "w" => 1, "diff1"=>1, "diff2"=>1, "diff3"=>1 , "triggered"=> 0, "neighbors" => []}}
+  end
+
+  def handle_cast({:set_neighbors, neighbors}, state) do
+    {:noreply, Map.put(state, "neighbors", neighbors)}
+  end
+
+  def handle_call({:get_neighbors}, _from, state) do
+    {:reply, Map.fetch(state, "neighbors"), state}
+  end
+
+  def handle_cast({:transmit_values}, state) do
+    {:ok, s} = Map.fetch(state, "s")
+    {:ok, w} = Map.fetch(state, "w")
+    # the following commented if branch will also hold good, depending on how you look at the problem, triggering one after the other, and
+    # the chain reaction starts by itself (this is what is implemented) or a guys being picked by main, followed triggering all the actors at once
+    # if length(neighbors) > 0 do
+    {:ok, diff1} = Map.fetch(state, "diff1")
+    {:ok, diff2} = Map.fetch(state, "diff2")
+    {:ok, diff3} = Map.fetch(state, "diff3")
+
+    if length(neighbors) > 0 && triggered!= 0 do
+        _ = GenServer.cast(Enum.random(neighbors), {:receive_values, s, w, self()})
+    end
+    {:noreply, state}
+  end
+
+  def handle_cast({:receive_values, s_recvd, w_recvd, sender}, state) do
+    {:ok, diff1} = Map.fetch(state, "diff1")
+    {:ok, diff2} = Map.fetch(state, "diff2")
+    {:ok, diff3} = Map.fetch(state, "diff3")
+
+    #if three consecutive differences are less than 10 power -10 terminate the actor
+    if (diff1 < :math.pow(10, -10) && diff2 < :math.pow(10, -10) && diff3 < :math.pow(10, -10)) do
+      _ = GenServer.cast(sender, {:terminate_neighbor, self()})
+      {:noreply, state}
+    else
+
+      # fetching current values and updating them
+      s_old = Map.fetch(state, "s")
+      w_old = Map.fetch(state, "w")
+      s_new = s_old + s_recvd
+      w_new = w_old + w_recvd
+
+      diff1 = diff2
+      diff2 = diff3
+      diff3 = s_new/w_new
+
+      #To keep half the value and send the other half
+      s_new = s_new/2
+      w_new = w_new/2
+
+
+      state = Map.put(state, "s", s_new)
+      state = Map.put(state, "w", w_new)
+      state = Map.put(state, "diff1", diff1)
+      state = Map.put(state, "diff2", diff2)
+      state = Map.put(state, "diff3", diff3)
+    end
+  end
+
+  def handle_cast({:terminate_neighbor, neighbor}, state) do
+    {:ok, neighbors} = Map.fetch(state, "neighbors")
+    {:noreply, Map.put(state, "neighbors", List.delete(neighbors, neighbor))}
+  end
+
+  def handle_call({:get_diff}, _from, state) do
+    {:reply, Map.fetch(state, "count"), state}
+  end
+
+  def handle_call({:set_message}, _from, state) do
+    {:reply, Map.put(state, "message", state)}
+  end
+
+  def set_neighbors(actor, neighbors) do
+    GenServer.cast(actor, {:set_neighbors, neighbors})
+  end
+end
--- proj2/lib/gossip.ex
+++ proj2/lib/gossip.ex
@@ -4,19 +4,14 @@ defmodule Gossip do
 
   def main do
     argument_list = System.argv()
-    # num_nodes = 0
 
-    # if Enum.count(argument_list) != 3 do
-    #   IO.puts("Provide number of nodes, topology and algorithm")
-    # else
-    #   num_nodes = Enum.at(argument_list, 0)
-    #   topology = Enum.at(argument_list, 1)
-    #   algorithm = Enum.at(argument_list, 2)
-    # end
-
-    num_nodes = String.to_integer(Enum.at(argument_list, 0))
-    topology = Enum.at(argument_list, 1)
-    algorithm = Enum.at(argument_list, 2)
+    if Enum.count(argument_list) != 3 do
+      IO.puts("Provide number of nodes, topology and algorithm")
+    else
+      num_nodes = String.to_integer(Enum.at(argument_list, 0))
+      topology = Enum.at(argument_list, 1)
+      algorithm = Enum.at(argument_list, 2)
+    end
 
     if num_nodes <= 1 do
       IO.puts("Nodes should be greater than 1")
@@ -39,6 +34,19 @@ defmodule Gossip do
       end_time = :os.system_time(:millisecond)
       IO.puts("Time taken for convergence is #{end_time - start_time}ms")
     else
+      actors = spawn_pushsum_actors(num_nodes)
+      map_of_neighbors = Utils.get_neighbors(actors, topology)
+
+      # now set appropriate list of neighbors to each actor
+      for {actor, neighbors} <- map_of_neighbors do
+        GenServer.cast(actor, {:set_neighbors, neighbors})
+      end
+
+      start_time = :os.system_time(:millisecond)
+      start_pushsum(actors, map_of_neighbors)
+      end_time = :os.system_time(:millisecond)
+      IO.puts("Time taken for convergence is #{end_time - start_time}ms")
+
       # Call to pushsum actors goes here
     end
   end
@@ -55,6 +63,52 @@ defmodule Gossip do
         actor
       end
     end)
+  end
+
+  def spawn_pushsum_actors(num_nodes) do
+    random_initial_node = Enum.random(1..num_nodes)
+
+    Enum.map(1..num_nodes, fn n ->
+      if n == random_initial_node do
+        {:ok, actor} = GenServer.start_link(PushsumActor, [n,1])
+        actor
+      else
+        {:ok, actor} = GenServer.start_link(PushsumActor, [n,0])
+        actor
+    end)
+  end
+
+  def start_pushsum(actors, map_of_neighbors) do
+    #Ask each actor to start sending values
+    for {actor, neighbors} <- map_of_neighbors do
+      GenServer.cast(actor, {:transmit_values})
+    end
+
+    live_actors = get_alive_pushsum_actors(actors)
+
+    if length(live_actors)>1 do
+      map_of_neighbors =
+        Enum.filter(map_of_neighbors, fn {actor, _} -> Enum.member?(live_actors, actor) end)
+      start_pushsum(live_actors, map_of_neighbors)
+    else
+      IO.puts("Pushsum ends all actors are terminated")
+    end
+  end
+
+  def get_alive_pushsum_actors(actors) do
+    alive_actors =
+      Enum.map(actors, fn act ->
+        {:ok, neighbors_list} = GenServer.call(act, {:get_neighbors})
+        neighbors_count = length(neighbors_list)
+        {:ok, diff_list} = GenServer.call(act, {:get_diff})
+        diff1 = Enum.at(diff_list, 0)
+        diff2 = Enum.at(diff_list, 1)
+        diff3 = Enum.at(diff_list, 2)
+        if Process.alive?(act) && neighbors_count > 0 && (abs(diff1) > :math.pow(10, -10) || abs(diff2) > :math.pow(10, -10) || abs(diff3) > :math.pow(10, -10)) do
+          act
+        end
+      end)
+
   end
 
   def start_gossiping(actors, map_of_neighbors) do
