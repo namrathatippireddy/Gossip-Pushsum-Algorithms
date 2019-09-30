c7f94480167be0e3b09bd28cf72cf81059d301f4
namrathatippireddy <mgunn001@odu.edu>
2019-09-30 17:11:07 -0400

    Changed honeycomb spelling in Util

--- proj2/lib/Util.ex
+++ proj2/lib/Util.ex
@@ -4,50 +4,61 @@ defmodule Utils do
     case topology do
       "line" ->
         Topology.line(actors)
+
       "full" ->
         Topology.get_full_neighbors(actors)
-        # "rand2D" -> numNodes
+
+      # "rand2D" -> numNodes
       "3Dtorus" ->
         Topology.get_3Dtorus_neighbours(actors)
+
       "honeycomb" ->
         Topology.get_honeycomb_neighbours(actors)
-      "randhoneycomd" ->
+
+      "randhoneycomb" ->
         Topology.get_randhoneycomb_neighbours(actors)
     end
   end
 
-  def node_correction(num_nodes,topology) do
+  def node_correction(num_nodes, topology) do
     case topology do
-        "line" ->
-          num_nodes
-        "full" ->
-          num_nodes
-        "rand2D" ->
-          num_nodes
-        "3Dtorus" ->
-          num_nodes = get_next_cube(1,num_nodes)
-        "honeycomb" ->
-           num_nodes
-        "randhoneycomd" ->
-          num_nodes
+      "line" ->
+        num_nodes
+
+      "full" ->
+        num_nodes
+
+      "rand2D" ->
+        num_nodes
+
+      "3Dtorus" ->
+        num_nodes = get_next_cube(1, num_nodes)
+
+      "honeycomb" ->
+        num_nodes
+
+      "randhoneycomb" ->
+        num_nodes
     end
   end
 
-  def get_next_cube(i,numNodes) do
-    test = (i*i*i)<numNodes
+  def get_next_cube(i, numNodes) do
+    test = i * i * i < numNodes
+
     if test do
-      get_next_cube(i+1,numNodes)
+      get_next_cube(i + 1, numNodes)
     else
-      i*i*i
+      i * i * i
     end
   end
 
-  def findCubeRoot(i,numNodes) do
-    test = (i*i*i) == numNodes
+  def findCubeRoot(i, numNodes) do
+    test = i * i * i == numNodes
+
     if test do
       i
     else
-      get_next_cube(i+1,numNodes)
+      get_next_cube(i + 1, numNodes)
     end
   end
 end
